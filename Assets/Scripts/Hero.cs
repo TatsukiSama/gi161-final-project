05a4a5cdcb46233fdbba5be18493038d9b6eca0a
Ninety001 <sanawuth.homc@bumail.net>
2025-11-30 18:08:12 +0700

    Finish Level 1 , Maintain 2

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,16 +2,23 @@ using UnityEngine;
 
 public class Hero : Character
 {
+    public Transform attackPoint;     
+    public GameObject slashEffectPrefab;  
+
     public override void TakeTurn(Character target)
     {
         Debug.Log($"{charName} โจมตี {target.charName}");
         target.TakeDamage(attack);
+
+        SpawnSlashEffect();
     }
 
     public void Attack(Character target)
     {
         Debug.Log($"{charName} ใช้โจมตีปกติ");
         target.TakeDamage(attack);
+
+        SpawnSlashEffect();
     }
 
     public void Guard()
@@ -30,7 +37,7 @@ public class Hero : Character
     public void UseSkill(Character target)
     {
         Debug.Log($"{charName} ใช้สกิล Slash!");
-        target.TakeDamage(attack + 10);
+        target.TakeDamage(attack + 20);
     }
 
     public void UseSkill(Character target, float multiplier)
@@ -38,6 +45,19 @@ public class Hero : Character
         int dmg = Mathf.RoundToInt(attack * multiplier);
         Debug.Log($"{charName} ใช้สกิล STRIKE x{multiplier}");
         target.TakeDamage(dmg);
+    }
+
+    public void SpawnSlashEffect()
+    {
+        if (slashEffectPrefab != null && attackPoint != null)
+        {
+            GameObject fx = Instantiate(slashEffectPrefab, attackPoint.position, Quaternion.identity);
+            Debug.Log("Effect Spawned at: " + attackPoint.position);
+            Debug.Log("AttackPoint world position = " + attackPoint.position);
+
+            Destroy(fx, 0.15f);
+
+        }
     }
 }
 
